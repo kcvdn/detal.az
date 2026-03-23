87a9bde6e09883baeb3cc01064ace412f66412e0
kcvdn <cavidankocariyev@gmail.com>
2026-03-23 16:12:48 +0400

    ilk versiya

---
+++ backend/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors(); // vacib
+
+  await app.listen(3002); // backend port
+}
+bootstrap();
